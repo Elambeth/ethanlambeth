344bc6cae2e81fec129658c68e47255f40ca00dc
Elambeth <elambeth@users.noreply.github.com>
2026-04-04 16:16:31 +1300

    changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -7,8 +7,5 @@ export const DATA = {
   url: "https://ethanlambeth.com", // Update this to your actual domain
   location: "Dunedin, NZ",
   locationLink: "https://www.google.com/maps/place/dunedin",
-  description:
-    "Studying BComm in Economics, minoring in Philosophy. I love building things and helping people.",
-  summary:
-    "I am passionate about problem-solving, leadership, and creating positive change. I have a keen interest in governance structures and their impact on organizations and communities. I am currently pursuing a Bachelor of Commerce in Economics, with a minor in Philosophy. I am committed to applying knowledge to solving real-world problems. I'm now seeking further opportunities to leverage my experience and drive meaningful change.",
+  description: "Ethan Lambeth. At the moment I am building panaceaindex.com",
 } as const;
